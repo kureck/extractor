204c0c1cf1e2a4543d1447a6cab852198d41f726
ekureck <eric.kureck@walmart.com>
2015-05-14 17:14:50 -0300

    adds folder travel method

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -7,9 +7,16 @@ class Extractor:
         self.in_root_folder = in_root_folder
         self.out_root_folder = out_root_folder
 
-    def create_out_folder(self, out_root_folder):
+    def create_out_folder(self):
         try:
-            os.makedirs(out_root_folder)
+            os.makedirs(self.out_root_folder)
         except OSError as exception:
             if exception.errno is not errno.EEXIST:
                 raise
+
+    def folder_travel(self):
+        # Set the directory you want to start from
+        for dirName, subdirList, fileList in os.walk(self.in_root_folder):
+            print('Found directory: %s' % dirName)
+            for fname in fileList:
+                print('\t%s' % fname)
